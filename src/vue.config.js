0c061050e788c0c5605d89c059952d4d9f78c428
codeDevFC <felixcobby@gmail.com>
2025-03-09 23:47:09 +0000

    Fix build process and configure for Vercel deployment

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,10 +1,7 @@
 
-// vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/fc-product-configurator/'
     : '/',
-  configureWebpack: {
-    devtool: 'source-map'
-  }
+  outputDir: 'dist'
 }
